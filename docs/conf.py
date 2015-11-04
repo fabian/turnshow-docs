7f82cffe7e45214cdaba7a5d81e45915444c6336
Fabian Vogler <fabian@equivalence.ch>
2015-11-04 08:09:35 +0100

    Add docs

---
+++ docs/conf.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+master_doc = 'index'
+
+project = u'Turnshow'
+copyright = u'Turnende Vereine Niederrohrdorf'
+
+html_theme = 'sphinx_rtd_theme'
---
+++ docs/index.rst
@@ -0,0 +1,7 @@
+Technische Dokumentation Turnshow
+=================================
+
+.. toctree::
+   :maxdepth: 2
+
+   stage
